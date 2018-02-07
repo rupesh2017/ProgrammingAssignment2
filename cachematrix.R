22c1d516f48f76faaf7d88887cdfddd24492d0ab
Rupesh <rupesh.sak94@gmail.com>
2018-02-08 01:39:26 +0530

    function defined to compute and cache inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## <<- operator to change value of another environment
+## function has been defined inside another function to get caching feature (lexical scoping)
+## solve() function work only on singular function i.e whose determinant is zero
 
-## Write a short comment describing this function
+
+## makeCacheMatrix function is used to calculated and store the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+m<-NULL                    #store cache mean
+
+set<-function(y){
+x<<-y                      
+m<-NULL
+}
+get<-function()x
+setinverse<-function(solve) m<<-solve
+getinverse<-function() m
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cachesolve is to check if inverse of matrix have been calculated for same input
+## if yes return the cached value else compute the inverse using makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+if(!is.null(m))
+{
+message("getting cached data")
+return(m)         # if match found print and exit function
+}
+data<-x$get()
+m<-solve(data,...)
+x$setinverse(m)
+m
+
+
 }
